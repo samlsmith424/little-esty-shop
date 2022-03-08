cbb2eee7439cec0e23c59ec2f77a7977b7a88546
Sam Smith <samlsmith424@gmail.com>
2022-03-08 12:03:18 -0800

    Feat: refactor tests for user story 6

--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -17,6 +17,8 @@ RSpec.describe InvoiceItem, type: :model do
   describe 'relationships' do
     it { should belong_to :invoice }
     it { should belong_to :item }
+    it { should have_one(:merchant).through(:item) }
+    # it { should}
   end
 
   describe 'instance methods' do
